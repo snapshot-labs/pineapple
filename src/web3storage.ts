2e60bf8944e49099a109d8a3cf88d71a79eaa3b3
bonustrack <fabien@bonustrack.co>
2022-04-26 23:57:37 +0700

    Remove comments

--- src/web3storage.ts
+++ src/web3storage.ts
@@ -13,30 +13,3 @@ export async function set(json) {
   console.log(cid, provider, ms);
   return { cid, provider, ms };
 }
-
-/**
-CREATE TABLE votes_copy2 (
-  id VARCHAR(66) NOT NULL,
-  ipfs VARCHAR(64) NOT NULL,
-  voter VARCHAR(64) NOT NULL,
-  created INT(11) NOT NULL,
-  space VARCHAR(64) NOT NULL,
-  proposal VARCHAR(66) NOT NULL,
-  choice JSON NOT NULL,
-  metadata JSON NOT NULL,
-  vp DECIMAL(64,30) NOT NULL,
-  vp_by_strategy JSON NOT NULL,
-  vp_state VARCHAR(24) NOT NULL,
-  cb INT(11) NOT NULL,
-  PRIMARY KEY (voter, space, proposal),
-  UNIQUE KEY id (id),
-  INDEX ipfs (ipfs),
-  INDEX voter (voter),
-  INDEX created (created),
-  INDEX space (space),
-  INDEX proposal (proposal),
-  INDEX vp (vp),
-  INDEX vp_state (vp_state),
-  INDEX cb (cb)
-);
-*/
