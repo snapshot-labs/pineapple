53bbc8f106eb3a7322e6d4eee32f84f0f055bfd0
bonustrack <fabien@bonustrack.co>
2022-05-06 16:20:23 +0700

    Remove body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config';
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import rpc from './rpc';
 import { version } from '../package.json';
@@ -8,8 +7,8 @@ import { version } from '../package.json';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json({ limit: '4mb' }));
-app.use(bodyParser.urlencoded({ limit: '4mb', extended: false }));
+app.use(express.json({ limit: '4mb' }));
+app.use(express.urlencoded({ limit: '4mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.use('/', rpc);
 app.get('/', (req, res) => res.json({ version, port: PORT }));
