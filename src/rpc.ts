d2585cb6250c6d2ed76f3a90696064302ab4bd55
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-06-03 01:18:28 +0200

    feat: add Infura provider

--- src/rpc.ts
+++ src/rpc.ts
@@ -3,6 +3,7 @@ import Promise from 'bluebird';
 import { MAX, rpcError, rpcSuccess } from './utils';
 import { set as setPinata } from './providers/pinata';
 import { set as setFleek } from './providers/fleek';
+import { set as setInfura } from './providers/infura';
 import { set as setAws } from './aws';
 
 const router = express.Router();
@@ -12,7 +13,7 @@ router.post('/', async (req, res) => {
   try {
     const size = Buffer.from(JSON.stringify(params)).length;
     if (size > MAX) return rpcError(res, 500, 'too large', id);
-    const result = await Promise.any([setPinata(params), setFleek(params)]);
+    const result = await Promise.any([setPinata(params), setFleek(params), setInfura(params)]);
     await setAws(result.cid, params);
     console.log('Success', result.provider, 'size', size);
     result.size = size;
