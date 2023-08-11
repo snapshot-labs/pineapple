67ab2260c12fae6067ba38ae33035d2743b42c5b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-11 13:28:41 +0900

    fix: catch error when all promises reject

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import fetch from 'cross-fetch';
+import fetch from 'node-fetch';
 import Promise from 'bluebird';
 import gateways from './gateways.json';
 import { set, get } from './aws';
@@ -13,15 +13,32 @@ router.get('/ipfs/*', async (req, res) => {
   try {
     const cache = await get(`cache/${key}`);
     if (cache) return res.json(cache);
-    const json = await Promise.any(
-      gateways.map(gateway => {
-        const url = `https://${gateway}${req.originalUrl}`;
-        return fetch(url).then(res => res.json());
-      })
-    );
-    res.json(json);
-    const size = Buffer.from(JSON.stringify(json)).length;
-    if (size <= MAX) await set(`cache/${key}`, json);
+
+    let json;
+
+    try {
+      json = await Promise.any(
+        gateways.map(async gateway => {
+          const url = `https://${gateway}${req.originalUrl}`;
+          const response = await fetch(url, { timeout: 15e3 });
+
+          if (!response.ok) {
+            return Promise.reject(response.status);
+          }
+          return response.json();
+        })
+      );
+      res.json(json);
+    } catch (e: any) {
+      return res.status(400).json();
+    }
+
+    try {
+      const size = Buffer.from(JSON.stringify(json)).length;
+      if (size <= MAX) await set(`cache/${key}`, json);
+    } catch (e: any) {
+      capture(e);
+    }
   } catch (e) {
     capture(e);
     return res.status(500).json();
