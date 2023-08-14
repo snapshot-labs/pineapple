8d2e21554973b3f7ff77e3869d8978692281e9f3
Wan <495709+wa0x6e@users.noreply.github.com>
2023-08-14 17:57:26 +0900

    fix: instrument the number of open outgoing connections (#155)
    * fix: instrument the number of open outgoing connections
    
    * fix: add `finally` block to trigger instrumentation on promise settle

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,7 +5,11 @@ import { capture } from '@snapshot-labs/snapshot-sentry';
 import gateways from './gateways.json';
 import { set, get } from './aws';
 import { MAX, sha256 } from './utils';
-import { ipfsGatewaysReturnCount, timeIpfsGatewaysResponse } from './metrics';
+import {
+  ipfsGatewaysReturnCount,
+  timeIpfsGatewaysResponse,
+  countOpenGatewaysRequest
+} from './metrics';
 
 const router = express.Router();
 
@@ -18,10 +22,18 @@ router.get('/ipfs/*', async (req, res) => {
     const result = await Promise.any(
       gateways.map(async gateway => {
         const end = timeIpfsGatewaysResponse.startTimer({ name: gateway });
-        const url = `https://${gateway}${req.originalUrl}`;
-        const response = await fetch(url);
-        end();
-        return { gateway, json: await response.json() };
+
+        try {
+          countOpenGatewaysRequest.inc({ name: gateway });
+
+          const url = `https://${gateway}${req.originalUrl}`;
+          const response = await fetch(url);
+
+          return { gateway, json: await response.json() };
+        } finally {
+          end();
+          countOpenGatewaysRequest.dec({ name: gateway });
+        }
       })
     );
     ipfsGatewaysReturnCount.inc({ name: result.gateway });
