b5a750795044a9824bd6264d471421484ee3df9b
Wan <495709+wa0x6e@users.noreply.github.com>
2023-09-07 11:14:21 +0900

    fix: IPFS gateway should return only json (#174)

--- test/e2e/proxy.test.ts
+++ test/e2e/proxy.test.ts
@@ -4,13 +4,21 @@ const HOST = `http://localhost:${process.env.PORT || 3003}`;
 
 describe('GET /ipfs/*', () => {
   describe('when the IPFS cid exists', () => {
-    it('returns the JSON file', async () => {
+    it('returns a JSON file', async () => {
       const response = await request(HOST).get(
         '/ipfs/bafkreib5epjzumf3omr7rth5mtcsz4ugcoh3ut4d46hx5xhwm4b3pqr2vi'
       );
 
       expect(response.statusCode).toBe(200);
       expect(response.body).toEqual({ status: 'OK' });
+    });
+
+    it('returns a 400 error when not a JSON file', async () => {
+      const response = await request(HOST).get(
+        '/ipfs/bafybeie2x4ptheqskiauhfz4w4pbq7o6742oupitganczhjanvffp2spti'
+      );
+
+      expect(response.statusCode).toBe(400);
     });
   });
 
