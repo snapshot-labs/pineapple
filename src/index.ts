ae114da8b4297eb5e99f155d39096b77eb278425
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-02-11 12:39:14 +0100

    chore: increase body size limit to 10MB

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ initLogger(app);
 initMetrics(app);
 
 app.disable('x-powered-by');
-app.use(express.json({ limit: '4mb' }));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.use(compression());
