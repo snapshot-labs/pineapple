4023c623585e4cde2296922572d35c85d45cf940
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-03-18 11:37:07 +0100

    feat: resolve files from TheGraph IPFS API (#222)
    We use TheGraph IPFS for EVM indexing, because other IPFS services
    are not resolving well with Subgraphs.
    
    This however becomes a problem, because TheGraph doesn't have public
    API, so:
    1. Trying to access proposal/vote metadata through browser (via View metadata) might not work.
    2. Migrating from subgraph to other indexer (Checkpoint) is problematic, because we might not be able
       to access old data.
    
    We can however access files from TheGraph's IFPS via API.

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,6 @@
 import { capture } from '@snapshot-labs/snapshot-sentry';
 import express from 'express';
+import { create } from 'ipfs-http-client';
 import fetch from 'node-fetch';
 import gateways from './gateways.json';
 import {
@@ -9,13 +10,34 @@ import {
 } from './metrics';
 import useProxyCache from './middlewares/useProxyCache';
 
-const router = express.Router();
 const UNSUPPORTED_FILE_TYPE = 'unsupported file type';
+
+const router = express.Router();
+const graphIpfsClient = create({ url: 'https://api.thegraph.com/ipfs/api/v0' });
+
+async function getFileFromGraphIpfs(cid: string) {
+  const chunks: Uint8Array[] = [];
+  for await (const chunk of graphIpfsClient.cat(cid)) {
+    chunks.push(chunk);
+  }
+
+  try {
+    const json = JSON.parse(Buffer.concat(chunks).toString('utf-8'));
+
+    return {
+      gateway: 'graph',
+      json
+    };
+  } catch (e) {
+    return Promise.reject(UNSUPPORTED_FILE_TYPE);
+  }
+}
 
 router.get('^/ipfs/:cid([0-9a-zA-Z]+)$', useProxyCache, async (req, res) => {
   try {
-    const result = await Promise.any(
-      gateways.map(async gateway => {
+    const result = await Promise.any([
+      getFileFromGraphIpfs(req.params.cid),
+      ...gateways.map(async gateway => {
         const end = timeIpfsGatewaysResponse.startTimer({ name: gateway });
         let status = 0;
 
@@ -47,7 +69,7 @@ router.get('^/ipfs/:cid([0-9a-zA-Z]+)$', useProxyCache, async (req, res) => {
           countOpenGatewaysRequest.dec({ name: gateway });
         }
       })
-    );
+    ]);
     ipfsGatewaysReturnCount.inc({ name: result.gateway });
 
     return res.json(result.json);
