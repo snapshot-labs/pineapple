9b67661b18b8661bbc190c1b031a28e840297b6b
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2025-06-20 17:49:54 +0400

    fix: remove redundant check

--- src/routes/proxy.ts
+++ src/routes/proxy.ts
@@ -6,25 +6,23 @@ import { Protocol } from '../proxies/types';
 
 const router = express.Router();
 
-const VALID_PROTOCOLS: Protocol[] = ['ipfs'];
+router.get<{ protocol: Protocol; cid: string }>(
+  '^/:protocol(ipfs)/:cid([0-9a-zA-Z]+)$',
+  useProxyCache,
+  async (req, res) => {
+    try {
+      const result = await resolveFromProxies(req.params.protocol, req.params.cid);
 
-router.get('^/:protocol(ipfs)/:cid([0-9a-zA-Z]+)$', useProxyCache, async (req, res) => {
-  if (!VALID_PROTOCOLS.includes(req.params.protocol as Protocol) || !req.params.cid) {
-    return res.status(400).json();
-  }
+      return res.json(result.json);
+    } catch (e) {
+      if (e instanceof AggregateError) {
+        return res.status(e.errors.includes(UNSUPPORTED_FILE_TYPE_ERROR) ? 415 : 400).json();
+      }
 
-  try {
-    const result = await resolveFromProxies(req.params.protocol as Protocol, req.params.cid);
-
-    return res.json(result.json);
-  } catch (e) {
-    if (e instanceof AggregateError) {
-      return res.status(e.errors.includes(UNSUPPORTED_FILE_TYPE_ERROR) ? 415 : 400).json();
+      capture(e);
+      return res.status(500).json();
     }
-
-    capture(e);
-    return res.status(500).json();
   }
-});
+);
 
 export default router;
